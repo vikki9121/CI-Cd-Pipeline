3538ea620021da98413e4feaad9c3a5c72f984a0
Vikki9121 <vignesh24csengineer@gmail.com>
2024-05-03 12:50:59 +0530

    third

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -10,7 +10,7 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-// GET UTC 11 good  time 
+// GET UTC 11 good  time work
 app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
 await app.RunAsync();
